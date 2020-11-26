ea41ee3bfcd240695dd19fe3ecde3d82ddf3f66d
sateesh <sateesh@redawning.com>
2020-11-26 13:07:34 +0530

    more tests for ConfigHelper

--- tests/unit/util_tests.py
+++ tests/unit/util_tests.py
@@ -19,6 +19,7 @@ class TestUtils(unittest.TestCase):
         self.assertEqual('value1', ConfigHelper.get_config_value('key1'))
         self.assertEqual('value2', ConfigHelper.get_config_value('key2'))
         self.assertEqual('value1+value2+value3', ConfigHelper.get_config_value('key3'))
+        self.assertEqual('value1+section1-value1+section2-value1+value_x', ConfigHelper.get_config_value('key_x', 'section3'))
 
         self.assertEqual('defaultvalue', ConfigHelper.get_config_value('missingkey', default_value='defaultvalue'))
         self.assertEqual('defaultvalue', ConfigHelper.get_config_value('missingkey', 'missingsection',
